dd20bdc3bbc7e48ad4f1f16ac2193ee939244cdb
dead plug <ivanovkirillalexandr@gmail.com>
2024-12-18 08:18:59 +0000

    Update config.py

--- lang-analyzer/config.py
+++ lang-analyzer/config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
 class Config:
-    def __init__(self, dump_dir="dumps", log_dir="logs"):
+    def __init__(self, dump_dir="lang-analyzer\\dumps", log_dir="lang-analyzer\\logs"):
         self.dump_dir = Path(dump_dir)
         self.log_dir = Path(log_dir)
